618511279d5a0f6293d2c6490c063959ddd67b49
paulakreft <kreftpaulinaa@gmail.com>
2021-01-31 22:02:04 +0100

    add button

--- app/src/components/Button.js
+++ app/src/components/Button.js
@@ -2,7 +2,7 @@ import Button from '@material-ui/core/Button';
 
 const MainButton = ({ title }) => {
     return (
-        <Button variant="contained" style={{'margin': "16px"}}>
+        <Button variant="contained" style={{'margin': "16px", 'backgroundColor': 'black', 'color':'white', 'fontWeight': '600'}}>
             {title}
         </Button>
 
